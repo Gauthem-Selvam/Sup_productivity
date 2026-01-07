0444670c64bfd0dbf6516a5dc981bb77464c2ae4
Gauthem Selvam <gauthem.selvam@ironmountain.com>
2026-01-07 06:30:03 +0530

    Add gitignore

--- app.py
+++ app.py
@@ -232,5 +232,5 @@ def send_huddle_email(
 # ======================
 
 if __name__ == "__main__":
-    print("🚀 Starting Huddle App...")
-    app.run(host="127.0.0.1", port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
